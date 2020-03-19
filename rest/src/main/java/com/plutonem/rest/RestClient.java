89541d47ac17ca496f1fa7544bdcbe8e671bb12d
Han Cao <1787873794@qq.com>
2020-03-20 03:20:37 +0800

    first.2 commit

--- rest/src/main/java/com/plutonem/rest/RestClient.java
+++ rest/src/main/java/com/plutonem/rest/RestClient.java
@@ -15,11 +15,11 @@ public class RestClient {
     public static enum REST_CLIENT_VERSIONS {V0, V1, V1_1, V1_2, V1_3}
     public static final String PARAMS_ENCODING = "UTF-8";
 
-    protected static final String REST_API_ENDPOINT_URL_V0 = "http://39.99.148.207/";
-    protected static final String REST_API_ENDPOINT_URL_V1 = "http://39.99.148.207/rest/v1/";
-    protected static final String REST_API_ENDPOINT_URL_V1_1 = "http://39.99.148.207/rest/v1.1/";
-    protected static final String REST_API_ENDPOINT_URL_V1_2 = "http://39.99.148.207/rest/v1.2/";
-    protected static final String REST_API_ENDPOINT_URL_V1_3 = "http://39.99.148.207/rest/v1.3/";
+    protected static final String REST_API_ENDPOINT_URL_V0 = "http://3.15.14.1/";
+    protected static final String REST_API_ENDPOINT_URL_V1 = "http://3.15.14.1/rest/v1/";
+    protected static final String REST_API_ENDPOINT_URL_V1_1 = "http://3.15.14.1/rest/v1.1/";
+    protected static final String REST_API_ENDPOINT_URL_V1_2 = "http://3.15.14.1/rest/v1.2/";
+    protected static final String REST_API_ENDPOINT_URL_V1_3 = "http://3.15.14.1/rest/v1.3/";
 
     private RequestQueue mQueue;
     private String mAccessToken;
